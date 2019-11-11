974cbe43939a2ff7cfd5cb870e5d8a68ecb8d8fd
Wen X. Pan <wen@mail.com>
2019-11-10 18:11:42 -0800

    Updates

---
+++ precache-manifest.7ee75d272d9c4325b9241e801c30039c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a5bb86f13ddf7574cb488486731b3c",
+    "url": "/react_demo/index.html"
+  },
+  {
+    "revision": "e86ecaf8eddee238e749",
+    "url": "/react_demo/static/css/main.9cb582d2.chunk.css"
+  },
+  {
+    "revision": "a20f2964f1e7103d046d",
+    "url": "/react_demo/static/js/2.3860c490.chunk.js"
+  },
+  {
+    "revision": "e86ecaf8eddee238e749",
+    "url": "/react_demo/static/js/main.774f254f.chunk.js"
+  },
+  {
+    "revision": "205e2557f96b41ce3852",
+    "url": "/react_demo/static/js/runtime-main.0f2dff43.js"
+  }
+]);
